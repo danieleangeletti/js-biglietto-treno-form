a691fe5d6bcc7e8168ee76a65b886f01824d8cf4
danieleangeletti <angelettidaniele@gmail.com>
2023-12-04 15:10:24 +0100

    milestone 1 completata

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,12 +1,21 @@
 const my_button = document.getElementById("calculation-button");
 
 my_button.addEventListener('click', function() {
-    const kilometres_input = document.getElementById("kilometres");
-    const kilometres_input_value = parseInt(kilometres_input.value);
+    let kilometres_input = document.getElementById("kilometres");
+    let kilometres_input_value = parseInt(kilometres_input.value);
 
-    const age_input = document.getElementById("age");
-    const age_input_value = parseInt(age_input.value);
+    let age_input = document.getElementById("age");
+    let age_input_value = parseInt(age_input.value);
 
-    console.log(kilometres_input_value);
-    console.log(age_input_value);
+    price = kilometres_input_value * 0.21;
+
+    if (age_input_value < 18) {
+        price = price * 0.8;
+    }
+
+    else if (age_input_value > 65) {
+        price = price * 0.6;
+    }
+
+    console.log(price.toFixed(2) + "€");
 })
