4e0d323faecf1154db662a76460f23c4784e4a15
danieleangeletti <angelettidaniele@gmail.com>
2023-12-04 16:28:01 +0100

    ADD HTML

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,21 +1,21 @@
 const my_button = document.getElementById("calculation-button");
 
 my_button.addEventListener('click', function() {
+    let name_and_surname = document.getElementById("name");
+    let name_and_surname_value = name_and_surname.value;
+
     let kilometres_input = document.getElementById("kilometres");
     let kilometres_input_value = parseInt(kilometres_input.value);
 
-    let age_input = document.getElementById("age");
-    let age_input_value = parseInt(age_input.value);
+    let age_range = document.getElementById("range");
+    let age_range_value = age_range.value;
 
     price = kilometres_input_value * 0.21;
 
-    if (age_input_value < 18) {
+    if (age_range_value = "minorenne") {
         price = price * 0.8;
     }
 
-    else if (age_input_value > 65) {
-        price = price * 0.6;
-    }
-
-    console.log(price.toFixed(2) + "€");
+    document.getElementById("passenger-name").innerHTML += name_and_surname_value;
+    document.getElementById("ticket-price").innerHTML += price + "€";
 })
